0f90fd4d439ff179f038b5011c1a533784a36b24
Matthew Reishus <mreishus@users.noreply.github.com>
2020-06-16 21:01:16 -0500

    Add .config/plasma-workspace/env/wm.sh

---
+++ .config/plasma-workspace/env/wm.sh
@@ -0,0 +1,13 @@
+# Disable KWin and use i3gaps as WM
+#export KDEWM=/usr/bin/i3
+# # Start Compton
+#picom &
+
+export KDEWM=/usr/bin/xmonad
+
+# Compositor (Animations, Shadows, Transparency)
+# xcompmgr -C
+# compton -b --config ~/.config/compton/compton.conf
+# On some configurations, `compton -b` may cause a display freeze. Use `compton &` instead
+# https://www.reddit.com/r/archlinux/comments/4ffsrt/compton_freezing_with_b/
+# https://wiki.archlinux.org/index.php/compton
