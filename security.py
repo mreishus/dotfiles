8c4f0ab1b95e730a76e579fee9cb3e9396f963e5
Matthew Reishus <mreishus@users.noreply.github.com>
2020-08-30 23:44:06 -0500

    Security updates

--- security.py
+++ security.py
@@ -6,10 +6,14 @@ progs_with_instructions = {
     "gobuster": "go get github.com/OJ/gobuster",
     "wfuzz": "yay -Syu wfuzz",
     "burpsuite": "yay -Syu burpsuite",
-    "nmap": "yay -Syu nmap",
+    "nmap": "yay -Syu nmap\nAlso install python2? Lots of scripts fail, not sure",
     "fzf": "yay -Syu fzf",
     "rg": "yay -Syu rg",
     "nc": "yay -Syu gnu-netcat",
+    "openvpn": "yay -Syu openvpn",
+    "searchsploit": "git clone https://github.com/offensive-security/exploitdb.git /opt/exploitdb\nln -sf /opt/exploitdb/searchsploit /usr/local/bin/searchsploit",
+    "msfdb": "yay -Syu metasploit\nThen set up postgres and make a DB by checking the arch wiki for both metasploit and postgres. (Not that hard, come on)",
+    "hydra": "yay -Syu hydra",
 }
 directories_with_instructions = {
     "/opt/seclists": "git clone --depth 1 https://github.com/danielmiessler/SecLists.git /opt/seclists" + "\nPossibly delete .git after to speed up shells",
@@ -21,7 +25,8 @@ if __name__ == "__main__":
             print(f"[+] {prog}")
         else:
             print(f"[-] MISSING: {prog}")
-            print("    " + instructions)
+            for line in instructions.split("\n"):
+                print("    " + line)
 
     for d, instructions in directories_with_instructions.items():
         if os.path.isdir(d):
