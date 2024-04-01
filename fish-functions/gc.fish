39168e23acfe9b3366f2446d48604f93366a0466
Matthew Reishus <mreishus@users.noreply.github.com>
2024-04-01 02:18:43 +0000

    gc, gca: add tz=utc0 env

--- fish-functions/gc.fish
+++ fish-functions/gc.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
 function gc --description 'alias gc=git commit --verbose'
-	git commit --verbose $argv;
+	env TZ=UTC0 git commit --verbose $argv;
 end
--- fish-functions/gca.fish
+++ fish-functions/gca.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
 function gca --description 'alias gca=git commit --verbose --all'
-	git commit --verbose --all $argv;
+	env TZ=UTC0 git commit --verbose --all $argv;
 end
